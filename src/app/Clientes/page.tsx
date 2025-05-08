d2ffdabeec8dfe165fa7c44a9bc87317b0e67a9d
Lorenzo Garcia <lorenzogdm0@gmail.com>
2025-05-08 10:14:59 -0400

    feat: ajustados para 20 50 e 100

--- src/app/Clientes/page.tsx
+++ src/app/Clientes/page.tsx
@@ -297,9 +297,9 @@ const ClientesPage: React.FC = () => {
               }}
               className={styles.itemsPerPageSelect}
             >
-              <option value={5}>5</option>
-              <option value={10}>10</option>
               <option value={20}>20</option>
+              <option value={50}>50</option>
+              <option value={100}>100</option>
             </select>
           </div>
         </div>
