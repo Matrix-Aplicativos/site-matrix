ee8eaf0ac499db3e830799c62f957e2ff1c9d9e9
Lorenzo Garcia <lorenzogdm0@gmail.com>
2025-08-20 22:06:49 -0400

    feat: ajustes

--- src/app/Painel-Coletas/RedefinirSenha/page.tsx
+++ src/app/Painel-Coletas/RedefinirSenha/page.tsx
@@ -4,7 +4,6 @@ import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 import Logo from "@/app/img/Logo.png";
-import "./RedefinirSenha.css";
 
 export default function RedefinirSenhaPage() {
   return (
